1d9e306dd8cb00fa272747b1be815846138afbce
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2023-08-16 19:02:10 +0700

    Update cgf.py

--- cgf.py
+++ cgf.py
@@ -1,3 +1,39 @@
+import os, sys
+
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+N = '\x1b[0m'    # WARNA MATI
+
+def clear():
+    if "win" in sys.platform:os.system("cls")
+    else:os.system("clear")
+
+try:
+    import requests
+except ImportError:
+    clear();print(f"\n [{M}!{N}] Modul {H}requests{N} belum terinstall!...\n")
+    os.system("pip install requests")
+
+try:
+    import bs4
+except ImportError:
+    clear();print(f"\n [{M}!{N}] Modul {H}Bs4{N} belum terinstall!...\n")
+    os.system("pip install bs4")
+
+try:
+    import rich
+except ImportError:
+    clear();print(f"\n [{M}!{N}] Modul {H}Rich{N} belum terinstall!..\n.")
+    os.system("pip install rich")
+
+try:
+    import base64
+except ImportError:
+    print("\n[\x1b[1;91m!\x1b[0m] tunggu sebentar sedang menginstall base64\n")
+    os.system("pip install base64")
+##############################################################################
 from hitaci import banteng
 
-banteng.prem_puan_puan_puan()
+if __name__=='__main__':
+    os.system("git pull")
+    banteng.prem_puan_puan_puan()
